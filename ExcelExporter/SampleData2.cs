e79bce21a56f96032d1c10de0b7b22bf66269c34
Scott Falbo <scottfalbo@microsoft.com>
2024-03-28 15:41:29 -0700

    added fields to models

--- ExcelExporter/SampleData2.cs
+++ ExcelExporter/SampleData2.cs
@@ -2,5 +2,13 @@
 
 internal class SampleData2
 {
+    public Guid Id { get; set; } = Guid.NewGuid();
 
+    public DateTimeOffset Date { get; set; } = DateTimeOffset.Now;
+
+    public decimal Amount { get; set; }
+
+    public bool HasSomething { get; set; }
+
+    public int Quantity { get; set; }
 }
