19daf8512b7da5e7fe0a302a600539632eacafcc
Scott Falbo <scottfalboart@gmail.com>
2024-03-28 15:31:32 -0700

    added sample data

--- ExcelExporter/SampleData1.cs
+++ ExcelExporter/SampleData1.cs
@@ -2,5 +2,13 @@
 
 internal class SampleData1
 {
+    public Guid Id { get; set; } = Guid.NewGuid();
 
+    public DateTimeOffset Date { get; set; } = DateTimeOffset.Now;
+
+    public decimal Amount { get; set; }
+
+    public bool IsWhatever { get; set; }
+
+    public string Description { get; set; }
 }
